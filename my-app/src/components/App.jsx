81c8cea3911351ab21f92bddc6f7607dc22e5403
Korobass87 <korobass1987@yandex.ru>
2022-04-06 00:57:05 +0300

    1

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics'
+import Section from './Section/Section'
+import Notification from './Notification/Notification'
+
+const feed = ["good", "neutral", "bad" ]
+
+
+export default function App () {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  function addFeed (e) {
+    let feedName = e.target.name
+    
+    // рабочий вариант, но не знаю можно ли так делать
+    // eval(`set${feedName[0].toUpperCase() + feedName.slice(1)}`)(eval(feedName) + 1)
+    
+    switch (feedName) {
+      case "good":
+        setGood(good+1);
+        break;
+      case "bad":
+      setBad(bad+1);
+      break;
+      case "neutral":
+        setNeutral(neutral+1);
+      break;    
+      default:
+        break;
+    }}
+  
+  
+  return (
+    <>
+    <Section title="Please leave feedback">
+      <FeedbackOptions options={feed} addFeed={addFeed} />  
+    </Section>
+    <Section title="Statistics">
+      {(good > 0 || neutral > 0 || bad > 0) ?
+      <Statistics good={good} neutral={neutral} bad={bad}/> 
+      : <Notification message="There is no feedback"></Notification> }          
+    </Section>
+    
+    
+    </>
+    
+    
+        )
+      }
+    
+  
+
+
---
+++ my-app/src/components/Statistics/Statistics.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Statistics.css'
+
+
+
+export default function Statistics ({good, neutral, bad}) {
+   
+   
+   let total = good + bad + neutral
+   let positiveFeddback = Math.trunc(good/total*100)
+   return (
+       <>
+        
+        <ul>
+            <li>
+                <h3>Good: <span>{good}</span></h3>
+            </li>
+            <li>
+                <h3>Neutral: <span>{neutral}</span></h3>
+            </li>
+            <li>
+                <h3>Bad: <span>{bad}</span></h3>
+            </li>
+            <li>
+                <h3>Total: <span>{total}</span> </h3>
+            </li>
+            <li>
+                <h3>Positive feddback: <span>{positiveFeddback}</span> % </h3>
+            </li>
+        </ul>
+
+       </>
+    )
+}
+
